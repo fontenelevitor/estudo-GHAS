17fd02440cdb1f310a4ee65fa7f67dc2c2fb5437
Vítor Fontenele <132603905+fontenelevitor@users.noreply.github.com>
2026-03-19 16:47:02 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,16 @@
 
-rom src.app import main
+# tests/test_app.py
+from src.app import main
 
 def test_main_runs(capsys):
+    # Executa a função principal
     main()
+
+    # Captura a saída do console
     captured = capsys.readouterr()
+
+    # Verifica se as mensagens esperadas foram impressas
     assert "Matriz 3x3:" in captured.out
     assert "Status GitHub API:" in captured.out
+out
 
