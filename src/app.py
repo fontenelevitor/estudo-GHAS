778cff35bc9085f98b87ff2f6674b0039a87ad9b
Vítor Fontenele <132603905+fontenelevitor@users.noreply.github.com>
2026-03-20 13:13:17 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+# src/app.py
 import numpy as np
 import requests
 
@@ -5,9 +6,5 @@ def main():
     arr = np.arange(1, 10).reshape(3, 3)
     print("Matriz 3x3:")
     print(arr)
-
     r = requests.get("https://api.github.com", timeout=10)
     print("Status GitHub API:", r.status_code)
-
-if __name__ == "__main__":
-    main()
